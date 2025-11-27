2ac572ea7d5ca6894791cc2e7e80a4828d6549cb
CoderShivam2005 <sm6718858@gmail.com>
2025-11-27 18:10:39 +0530

    removed unnecessary dile

--- f.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-url = "https://www.google.com"
-response = urllib.request.urlopen(url)
-html = response.read().decode('utf-8')
-
-print(html[:200])  # print first 200 characters of the page
